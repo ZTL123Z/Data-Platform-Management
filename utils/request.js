7c22d37d68583f0c72c757fb409987896d7dfa99
ZTL123Z <2746395413@qq.com>
2024-12-29 19:53:21 +0800

    axios响应拦截器和身份验证失败

--- utils/request.js
+++ utils/request.js
@@ -9,3 +9,19 @@ axios.interceptors.request.use(function (config) {
     )
     return config;
 })
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response;
+}, function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    if (error?.response?.status === 401) {
+        alert('登录过期，请重新登录')
+        localStorage.clear()
+        location.href = '../pages/login/index.html'
+    }
+    return Promise.reject(error);
+});
