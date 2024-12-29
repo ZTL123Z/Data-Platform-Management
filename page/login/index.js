3c26db1b159af1ad2f48f816656bdee69c587e3c
ZTL123Z <2746395413@qq.com>
2024-12-29 18:38:37 +0800

    验证码登入

--- page/login/index.js
+++ page/login/index.js
@@ -5,3 +5,23 @@
  * 1.3 基于 axios 调用验证码登录接口
  * 1.4 使用 Bootstrap 的 Alert 警告框反馈结果给用户
  */
+
+// 1.2 收集手机号和验证码数据
+document.querySelector('.login-form').addEventListener('click', (e) => {
+    const form = document.querySelector('.login-form')
+    const data = serialize(form, { hash: true, empty: true })
+    axios({
+        url: '/v1_0/authorizations',
+        method: 'post',
+        data: {
+            "mobile": data.mobile,
+            "code": data.code
+        }
+    }).then(res => {
+        myAlert(true, '登录成功')
+        console.log(res)
+    }).catch(err => {
+        myAlert(false, '登录失败')
+        console.log(err)
+    })
+})
--- utils/request.js
+++ utils/request.js
@@ -1,2 +1,3 @@
 // axios 公共配置
 // 基地址
+axios.defaults.baseURL = 'http://geek.itheima.net'
